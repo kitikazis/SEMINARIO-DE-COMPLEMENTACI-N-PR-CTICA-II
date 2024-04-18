f5a2b07938f86863a1d965e8aa226fabda797ece
Yamir <luiskitikazis2@gmail.com>
2024-04-17 19:31:27 -0500

    first commit

--- paginas/controlador/ctr_borrar_cli.php
+++ paginas/controlador/ctr_borrar_cli.php
@@ -5,7 +5,7 @@ $crudcliente = new CRUDCliente();
 
 if (isset($_GET['codcli'])) {
     $codcli = $_GET["codcli"];
-
+//
     $crudcliente->BorrarCliente($codcli);
 
     header('location: ../vista/listar_clientes.php');
